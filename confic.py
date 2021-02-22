a2c4aeac379ee9c45ce3844493873127ba41b3c6
Mykola <m.zaulychny@gmail.com>
2021-02-22 11:08:39 +0200

    Create confic.py

---
+++ confic.py
@@ -0,0 +1,14 @@
+TOKEN = 'NzgzNjM0MjQzNTQ0MDIzMDQw.X8dmQg.nAiraNSkuQgdcNhFrSI7ZnTMw5E' # bot token
+POST_ID = 783651381625487390 # post id to read reactions from
+
+ROLES = {
+   '🎮': 783645060742512641 #gamer
+   '🖥️': 783645168527998986 #programer
+   '🕋': 783645368855691279 #3D-dev
+   '👶': 783645993962045451 #new
+   '🦑': 783652149141700618 #anime
+}
+
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 5
